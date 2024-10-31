9ea1ed2b0aa892bd88b0808905142e71087817f4
mmasaki0 <111476395+mmasaki0@users.noreply.github.com>
2024-10-31 00:12:50 -0500

    testing flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, SafeAreaView, Appearance, useColorScheme } from "react-native"
+import { View, Text, StyleSheet, SafeAreaView, Appearance, useColorScheme, FlatList } from "react-native"
 import { useEffect, useState } from "react"
 
 const index = () => {
@@ -6,8 +6,37 @@ const index = () => {
     return (
         <SafeAreaView>
             <Text>Home</Text>
+            <FlatList
+                data = {[
+                    {id: "1", title: "test"}, 
+                    {id: "2", title: "test2"},
+                    {id: "3", title: "test3"},
+                ]}
+                keyExtractor={(item) => item.id}  
+                renderItem={ ({item}) => (
+                    <View>
+                        <Text style={styles.listContainer}>{item.title}</Text>
+                    </View>
+                )}  
+                horizontal={true}
+                snapToAlignment = "start"
+                decelerationRate={"fast"}
+                snapToInterval={500}
+                showsHorizontalScrollIndicator={false}
+            />
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    listContainer: {
+        width: 500,
+        height: 500,
+        backgroundColor: "gray",
+        padding: 10,
+        borderColor: "black",
+        borderWidth: 5
+    }
+})
 
 export default index
