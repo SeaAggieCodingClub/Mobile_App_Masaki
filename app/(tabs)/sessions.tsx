ad4f001f00d768f0df5369c66cec02508f16f688
mmasaki0 <111476395+mmasaki0@users.noreply.github.com>
2025-02-04 19:00:35 -0600

    started sessions tab

--- app/(tabs)/sessions.tsx
+++ app/(tabs)/sessions.tsx
@@ -1,11 +1,30 @@
-import { View, Text, StyleSheet, SafeAreaView, Appearance, useColorScheme } from "react-native"
+import { View, Text, StyleSheet, FlatList, Pressable, SafeAreaView, Appearance, useColorScheme } from "react-native"
 import { useEffect, useState } from "react"
 import globalStyles from "../globalStyles"
+
+interface sessionsProps {
+    name: string,
+    day: Array<string>,
+}
+
 const sessions = () => {
+    const sessions = [{name: "test1", day: ["M"]}, {name: "test2", day: ["T", "Th"]}]
     
     return (
         <SafeAreaView style={globalStyles.androidSafeView}>
             <Text style={globalStyles.pageTitle}>Sessions</Text>
+
+            <FlatList
+                style={{}}
+                data={sessions}
+                keyExtractor={(item) => item.name} 
+                renderItem={({item}: {item: sessionsProps})=>(
+                    <Pressable style={globalStyles.tile}>
+                        <Text>{item.name}</Text>
+                    </Pressable>
+                )}
+            />
+
         </SafeAreaView>
     )  
 }
