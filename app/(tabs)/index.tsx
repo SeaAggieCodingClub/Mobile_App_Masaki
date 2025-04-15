6775865381b23ba755e1f4bc7d23b304e23fa6ef
CatPigeon3 <sharaf.mehmood.hex@gmail.com>
2025-04-15 15:34:53 -0500

    Styled homepage a bit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -24,13 +24,21 @@ const index = () => {
         }, [])
     )
 
-    const todaySessions = userSessions.filter(userSessions => (userSessions.daysOfSession.includes(date.toLowerCase())))
+    const todaySessions = userSessions.filter(userSessions => (userSessions.daysOfSession.includes("thursday")))
 
     return (
         <SafeAreaView style={globalStyles.androidSafeView}>
-                <Text style={globalStyles.pageTitle}>Home</Text>
+                <Text style={[globalStyles.pageTitle, {backgroundColor: styleColors.darkest, marginBottom: 10}]}>Home</Text>
+                <View style={{backgroundColor: styleColors.darkest, borderColor: styleColors.dark, borderWidth: 1, borderRadius: 8, padding: 8, marginHorizontal: 8, height: 695}}>
+                    <View style={{padding: 25, marginHorizontal: "auto", paddingTop: -5}}>
+                        <Text style={styles.listText}>
+                            {date}
+                        </Text>
+                    </View>
                 
-                <FlatList
+                {//<View style={{height: 2, marginHorizontal: 16, backgroundColor: "rgba(255, 255, 255, 0.25)", marginBottom: 12}}></View>
+                }
+                {/* <FlatList
                     style={{borderRadius: styles.listContainer.borderRadius, marginLeft: 16, marginRight: 16}}
                     data = {[
                         {id: "1", title: date}, 
@@ -49,11 +57,11 @@ const index = () => {
                     snapToInterval={styles.listContainer.width + 20}
                     //snapToInterval={Dimensions.get("window").width - 2 * globalStyles.screenContainer.paddingLeft}
                     showsHorizontalScrollIndicator={false}
-                    contentContainerStyle={{columnGap: 20}}
+                    contentContainerStyle={{columnGap: 20, marginBottom: 10}}
                     
-                />
+                /> */}
                 <FlatList
-                    style={{paddingHorizontal: 16, paddingTop: 10}}
+                    style={{paddingHorizontal: 16}}
                     numColumns={1}
                     contentContainerStyle={{gap: 8}}
                     //columnWrapperStyle={{gap: 8}}
@@ -74,6 +82,7 @@ const index = () => {
                     </Pressable>
                     )}
                 />
+                </View>
         </SafeAreaView>
     )
 }
@@ -81,18 +90,19 @@ const index = () => {
 const styles = StyleSheet.create({
     listContainer: {
         width: Dimensions.get("window").width - 2 * globalStyles.screenContainer.paddingLeft,
-        height: 75,
+        //height: 75,
         backgroundColor: styleColors.dark,
         borderColor: styleColors.light,
         borderWidth: 1,
         padding: 10,
         borderRadius: 10,
+        marginBottom: 10
     },
 
     listText: {
         color: styleColors.light,
         fontFamily: "Montserrat-Medium",
-        fontSize: 25,
+        fontSize: 20,
     }
 })
 
